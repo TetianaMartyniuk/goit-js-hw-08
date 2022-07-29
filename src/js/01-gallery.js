d98720fd507c6cf83ec1aa5179cdb968f012ec94
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-07-29 15:06:37 +0300

    setup simplesightbox and task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkUp = galleryItems.map((item) => {
+    const markUp = `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    return markUp;
+}).join("");
+gallery.insertAdjacentHTML('beforeend', galleryMarkUp);
+// console.log(galleryItems);
+
+const image = document.querySelectorAll(".gallery__image")
+// console.dir(image);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "outside",
+    captionDelay: 250,
+});
+console.dir(lightbox);
