2554ed0ba1f8a74014eae9e391ee50e4cb9527bc
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-08-05 18:54:37 +0300

    task 3 + update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,28 +6,27 @@ const player = new Player(iframe);
 const key = 'videoplayer-current-time';
 
  function onPlay(event) {
-    addToStorage(event);
-
-    const playedSeconds = getFromStorage().seconds;
-    const savedTime = Math.floor(playedSeconds);
-    // console.log(playedSeconds);
-    return savedTime
+    localStorage.setItem(key, event.seconds);
 
 }
 
+player.on('timeupdate', throttle(onPlay, 1000));
+let time = getFromStorage();
+console.log(time);
 
-let time = player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(time).then(function(seconds) {}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
-player.setCurrentTime(time).then(function(seconds) {}).catch(function(error) {});
-
-
-
-function addToStorage(videoTime) {
-    localStorage.setItem(key, JSON.stringify(videoTime));
-}
 function getFromStorage() {
     const getLocalData = localStorage.getItem(key);
-    const localData = JSON.parse(getLocalData);
-    return localData
+    return getLocalData
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,71 @@
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
-import { throttle } from "lodash";
+//1 Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з 
+//полями email і message, у яких зберігай поточні значення полів форми.
+//2 Нехай ключем для сховища буде рядок "feedback-form-state".
+//3 Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, 
+//заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
+//4 Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль 
+//об'єкт з полями email, message та їхніми поточними значеннями.
+//5 Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. 
+//Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+import { add, throttle } from "lodash";
 
 const feedbackForm = document.querySelector(".feedback-form");
-const key = "feedback-form-state";
+const formInput = document.querySelector("input");
+const formText = document.querySelector("textarea");
 
-feedbackForm.addEventListener("input", (event) => {console.log(event.target);})
+const key = "feedback-form-state";
+// console.log(formInput);
+
+formUpdate();
+
+feedbackForm.addEventListener("input", throttle(addToStorage, 1000));
+feedbackForm.addEventListener("submit", clearForm);
+
+
+function addToStorage(event) {
+    event.preventDefault();
+    // console.dir(event);
+    const newData = event.currentTarget.elements;
+    let dataObj = {};
+    dataObj.email = newData.email.value;
+    dataObj.message = newData.message.value;
+    localStorage.setItem(key, JSON.stringify(dataObj))
+
+}
+function clearForm(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(key)
+    // if (formInput.value !== "" && formText.value !== "") {
+    //     formInput.value = ""
+    //     formText.value = ""
+    // }
+}
+
+function formUpdate() {
+    const getData = JSON.parse(localStorage.getItem(key));
+    // console.log(getData);
+    formInput.value = getData.email;
+    formText.value = getData.message;
+
+}
+
+
+
+
+
+
+// let getEmail = (event) => {
+//     if (event.target.type === "email") {
+//         return emailValue = event.target.value;
+//     }
+//     if (event.target.type !== "textarea") {
+//         return messageValue = event.target.value;
+//     }
+//     let formText = {
+//         email: emailValue,
+//         message: messageValue,
+//     }
+
+//     return formText
+// }
